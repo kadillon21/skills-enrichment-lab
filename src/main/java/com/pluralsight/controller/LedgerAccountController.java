54e954981a30634f240caf86678bce44210e8ca7
kadillon21 <kadillon21@gmail.com>
2026-07-14 15:23:00 -0500

    Feature: GET Method for api/transactions implemented. Also started to implement other endpoints for api/transactions

---
+++ src/main/java/com/pluralsight/controller/LedgerAccountController.java
@@ -0,0 +1,44 @@
+package com.pluralsight.controller;
+
+import com.pluralsight.model.LedgerAccount;
+import com.pluralsight.service.LedgerAccountService;
+import com.pluralsight.repository.LedgerAccountRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ledger-accounts")
+public class LedgerAccountController {
+    
+    private LedgerAccountService ledgerAccountService;
+    
+    public LedgerAccountController(LedgerAccountService ledgerAccountService) {
+        this.ledgerAccountService = ledgerAccountService;
+    }
+    
+//     @GetMapping("")
+//     public ResponseEntity<List<LedgerAccount>> getAll() {
+//         return ledgerAccountService.getAll();
+//     }
+//
+//     @PostMapping("")
+//     public LedgerAccount create(LedgerAccount ledgerAccount) {
+//         return ledgerAccountService.create(ledgerAccount);
+//     }
+//
+//     @PutMapping("")
+//     public ResponseEntity<LedgerAccount> update(LedgerAccount ledgerAccount) {
+//         return ledgerAccountService.update(ledgerAccount);
+//     }
+//
+//     @DeleteMapping("/{id}")
+//     public ResponseEntity<Void> delete(Long id) {
+//         return ledgerAccountService.delete(id);
+// }
+}
---
+++ src/main/java/com/pluralsight/service/LedgerAccountService.java
@@ -0,0 +1,22 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.LedgerAccount;
+import com.pluralsight.repository.LedgerAccountRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class LedgerAccountService {
+    private LedgerAccountRepository ledgerAccountRepository;
+
+    public LedgerAccountService(LedgerAccountRepository ledgerAccountRepository) {
+        this.ledgerAccountRepository = ledgerAccountRepository;
+    }
+
+    public List<LedgerAccount> getAll() {
+        return ledgerAccountRepository.findAll();
+    }
+}
